8b2694e4a93c047d09270a6d8b982828c8ed8ee8
Nicola Stanislao Vitale <nivit@rocketmail.com>
2020-06-27 08:33:54 +0200

    Fix bug with RSS feeds that don't use entry.link
    Add also a check of the URL (must start with https?://).

--- download_rss_feeds.py
+++ download_rss_feeds.py
@@ -8,6 +8,14 @@ and url of every RSS item in another file.
 import sys
 import feedparser
 
+
+def is_uri(path):
+    """
+    Check if path has a protocol https?://
+    """
+    return bool(path.startswith('http://') or path.startswith('https://'))
+
+
 if __name__ == '__main__':
 
     if len(sys.argv) == 3:
@@ -17,6 +25,7 @@ if __name__ == '__main__':
         FN_ITEMS = sys.argv[2]
     else:
         print("usage: python {} input_file output_file".format(sys.argv[0]))
+        sys.exit(0)
 
     F_ITEMS = open(FN_ITEMS, 'w')
 
@@ -24,17 +33,32 @@ if __name__ == '__main__':
         FEEDS = f_feeds.readlines()
 
         for url in FEEDS:
-            if url.startswith('http://') or url.startswith('https://'):
+            if is_uri(url):
                 feed = feedparser.parse(url)
             else:
                 continue
 
-            site = feed.feed.title
+            try:
+                site = feed.feed.title
+            except AttributeError:
+                continue
 
             for entry in feed.entries:
                 F_ITEMS.write("{}\n".format(site))
                 F_ITEMS.write("{}\n".format(entry.title))
-                F_ITEMS.write("{}\n".format(entry.link))
+                #F_ITEMS.write("{}\n".format(entry.guid))
+
+                try:
+                    if entry.guid and is_uri(entry.guid):
+                        F_ITEMS.write("{}\n".format(entry.guid))
+                    elif entry.link and is_uri(entry.link):
+                        F_ITEMS.write("{}\n".format(entry.link))
+                except (KeyError, AttributeError):
+                    try:
+                        if entry.link and is_uri(entry.link):
+                            F_ITEMS.write("{}\n".format(entry.link))
+                    except (KeyError, AttributeError):
+                        F_ITEMS.write("{}\n".format(feed.feed.link))
 
     del F_ITEMS
 
