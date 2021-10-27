780c6d699ea59071b12071ed5d0974f75bf83828
Nicola Stanislao Vitale <nivit@rocketmail.com>
2021-10-27 08:57:17 +0200

    Fix installation in Linux

--- install.sh
+++ install.sh
@@ -12,9 +12,9 @@ now="$(date +%Y-%m-%dT%H:%M:%S)"
 
 if [ ! -d "${destdir}" ]; then
 
-  install -U -d "${destdir}"
-  install -U -d "${destdir}/conf"
-  install -U -b -m 644 ./*.conf ./*.rasi feeds_list* "${destdir}/conf"
+  install -d "${destdir}"
+  install -d "${destdir}/conf"
+  install -b -m 644 ./*.conf ./*.rasi feeds_list* "${destdir}/conf"
 
   if [ -f "${polybar_conf}" ] && \
       ! /usr/bin/grep -q '^; \[module/news\]' "${polybar_conf}"; then
@@ -28,34 +28,34 @@ else  # update
   cp -Rf "${destdir}" "${destdir}.${now}"
   echo "-- Backup copy created in ${destdir}.${now} --"
 
-  install -U -d "${destdir}/conf"
-  install -U -b -m 644 conf/feeds_list_breaking_news "${destdir}"/conf/
+  install -d "${destdir}/conf"
+  install -b -m 644 conf/feeds_list_breaking_news "${destdir}"/conf/
 
   if [ -f "${destdir}"/config.rasi ] &&
       [ ! -f "${destdir}/conf/config.rasi" ]; then
-    sed -i '' -e 's,\(scripts/news\)\(/news-theme.rasi\),\1/conf\2,1' \
+    sed -i'' -e 's,\(scripts/news\)\(/news-theme.rasi\),\1/conf\2,1' \
       "$(realpath "${destdir}"/config.rasi)"
-    install -U -b -m 644 "${destdir}"/config.rasi "${destdir}"/conf/
+    install -b -m 644 "${destdir}"/config.rasi "${destdir}"/conf/
   fi
 
   if [ -f "${destdir}"/module.conf ] &&
-      [ ! -f "${destdir}"/module.conf ]; then
-    install -U -b -m 644 "${destdir}"/module.conf "${destdir}"/conf/
+      [ ! -f "${destdir}"/conf/module.conf ]; then
+    install -b -m 644 "${destdir}"/module.conf "${destdir}"/conf/
   fi
 
   if [ -f "${destdir}"/news-theme.rasi ] &&
       [ ! -f "${destdir}"/conf/news-theme.rasi ]; then
-    install -U -b -m 644 "${destdir}"/news-theme.rasi "${destdir}"/conf/
+    install -b -m 644 "${destdir}"/news-theme.rasi "${destdir}"/conf/
   fi
 
-  if [ -f "${destdir}"/rss.feeds ] && \
+  if [ -f "${destdir}"/rss.feeds ] &&
       [ ! -f "${destdir}/conf/feeds_list" ]; then
-    install -U -b -m 644 "${destdir}"/rss.feeds "${destdir}"/conf/feeds_list
+    install -b -m 644 "${destdir}"/rss.feeds "${destdir}"/conf/feeds_list
   fi
 
   if [ -f "${destdir}"/news.conf ]; then
-    install -U -b -m 644 "${destdir}"/news.conf "${destdir}"/conf/
-    sed -i '' -e 's,\(scripts/news\)\(/config.rasi\),\1/conf\2,1' \
+    install -b -m 644 "${destdir}"/news.conf "${destdir}"/conf/
+    sed -i'' -e 's,\(scripts/news\)\(/config.rasi\),\1/conf\2,1' \
         -e 's,add\(_ellipsis\),use\1,1' \
         -e 's,menu\(_prompt\),search\1,1' \
         "$(realpath "${destdir}"/conf/news.conf)"
@@ -68,14 +68,14 @@ else  # update
     if ! /usr/bin/grep -q '^; \[module/news\]' "${polybar_conf}"; then
       cat polybar.conf >> "${polybar_conf}"
     else
-      sed -i '' -e 's,\(scripts/news\)\(/module.conf\),\1/conf\2,1' \
+      sed -i'' -e 's,\(scripts/news\)\(/module.conf\),\1/conf\2,1' \
         "$(realpath "${polybar_conf}")"
     fi
   fi
 fi
 
-install -U -m 554 ./*.py "${destdir}"
-install -U -m 554 news.sh "${destdir}"
+install -m 554 ./*.py "${destdir}"
+install -m 554 news.sh "${destdir}"
 
 # clean up
 rm -f "${destdir}"/download_rss_feeds.py
